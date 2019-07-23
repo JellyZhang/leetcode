e225cb77e3ec337e5df96dcd6cff8fa58e93a852
zhangjelly <zhangjelly0405@gmail.com>
2019-07-24 01:27:31 +0800

    comment 35

--- 35.search-insert-position.cpp
+++ 35.search-insert-position.cpp
@@ -24,9 +24,19 @@ public:
             }
         }
         //target only possible in [p,q];
-        //when end with p=mid+1,means pos q is impossible, only possible in [p,+infinity); so insert at p;
+        //when end with p=mid+1(only happen when p==q==mid),means pos q is impossible, only possible in [p,+infinity); so insert at p;
         //when end with q=mid-1,like the former situation, but notice that when we insert at i,we push older num[i] to the right side.so in this situation ,
-        //target only possible in (-infinity,q], we should insert into p(which is q+1);
+        //target only possible in (-infinity,q], we should insert into p(which is q+1); 
+        //<first explaination may be not so good>
+        //
+        //second explaination:
+        //we can sure about this: poses that right to q,all greater than target ,poses that left to p,all lower than target.
+        //so when end like q p ,we can see as :
+        //.. .. .. .. .. q p .. .. .. .. ..
+        //l  l  l  l  l  l g g  g  g  g  g   (l means this pos lower than target ,g means greater)
+        //so clearly we should insert p;
+        //
+        //i think the second one is better.
         return p;
     }
 };
