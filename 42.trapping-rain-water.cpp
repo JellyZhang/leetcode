5f16a6675c07f03500955e153b2c777e0da9d174
zhangjelly <zhangjelly0405@gmail.com>
2019-08-18 02:28:50 +0800

    41,42

--- 42.trapping-rain-water.cpp
+++ 42.trapping-rain-water.cpp
@@ -1,37 +1,60 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int leftMax = 0;
-        int ans = 0;
-        int sum = 0;
-        bool increasing = true;
-        height.push_back(0);
-        height.insert(height.begin(),0);
-        for(int i =1;i<height.size();++i){
-//            cout<<sum<<","<<ans<<".";
-            cout<<height[i]<<",";
-            if(increasing && height[i]<height[i-1]){
-                if(height[i-1]<height[leftMax]){
-                    sum-=(i-1-leftMax)*(height[leftMax]-height[i-1]);
-                    int temp = leftMax+1;
-                    while(height[temp]>height[i-1]){
-                        sum+=height[temp]-height[i-1];
-                        ++temp;
-                    }
+        //int len = height.size();
+        //if(len==0){
+            //return 0;
+        //}
+        //vector<int> leftmax(len,0);
+        //vector<int> rightmax(len,0);
+        //int l_max=height[0];
+        //for(int i=0;i<len;++i){
+            //if(height[i]>l_max){
+                //l_max=height[i];
+            //}
+            //leftmax[i]=l_max;
+        //}
+        //int r_max=height[len-1];
+        //for(int i=len-1;i>=0;--i){
+            //if(height[i]>r_max){
+                //r_max=height[i];
+            //}
+            //rightmax[i]=r_max;
+        //}
+        //int ans=0;
+        //for(int i=0;i<len;++i){
+            //ans+=min(leftmax[i],rightmax[i])-height[i];
+        //}
+        //return ans;
+        int len = height.size();
+        if(len==0){
+            return 0;
+        }
+        int i=0;
+        int j=len-1;
+        int leftmax=height[0];
+        int rightmax=height[len-1];
+        int ans=0;
+        while(i<=j){
+            if(height[i]<=height[j]){
+                if(height[i]>leftmax){
+                    leftmax=height[i];
                 }
-                ans +=sum;
-                sum=0;
-                leftMax = i-1;
-                increasing = false;
+                else{
+                    ans+=leftmax-height[i];
+                }
+                ++i;
             }
-            if(height[i]<height[leftMax]){
-                sum+=height[leftMax]-height[i];
+            else{
+                if(height[j]>rightmax){
+                    rightmax=height[j];
+                }
+                else{
+                    ans+=rightmax-height[j];
+                }
+                --j;
             }
-            if(!increasing && height[i]>height[i-1]){
-                increasing = true;
-            }
-        } 
+        }
         return ans;
-        
     }
 };
